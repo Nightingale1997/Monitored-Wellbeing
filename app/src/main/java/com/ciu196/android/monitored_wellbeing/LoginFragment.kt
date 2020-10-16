473c1601823042f6ac085c7d7d78a87b59e6da45
Kevin Solovjov <kevin.solovjov@gmail.com>
2020-10-16 22:05:31 +0200

    Removed old LoginFragment and navigation to ChallengeFragment
    Removed old screen for login and put it in mainfragment, logged in users are sent to challengefragment.

--- app/src/main/java/com/ciu196/android/monitored_wellbeing/LoginFragment.kt
+++ app/src/main/java/com/ciu196/android/monitored_wellbeing/LoginFragment.kt
@@ -39,7 +39,7 @@ import com.google.firebase.auth.FirebaseAuth
 class LoginFragment : Fragment() {
 
     companion object {
-        const val TAG = "LoginFragment"
+        const val TAG = "MainFragment"
         const val SIGN_IN_RESULT_CODE = 1001
     }
 
@@ -57,7 +57,7 @@ class LoginFragment : Fragment() {
             inflater, R.layout.fragment_login, container, false
         )
 
-        binding.authButton.setOnClickListener { launchSignInFlow() }
+        //binding.authButton.setOnClickListener { launchSignInFlow() }
 
         return binding.root
     }
@@ -90,39 +90,4 @@ class LoginFragment : Fragment() {
         })
     }
 
-    private fun launchSignInFlow() {
-        // Give users the option to sign in / register with their email or Google account. If users
-        // choose to register with their email, they will need to create a password as well.
-        val providers = arrayListOf(
-            AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
-        )
-
-        // Create and launch sign-in intent. We listen to the response of this activity with the
-        // SIGN_IN_RESULT_CODE code.
-        startActivityForResult(
-            AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(
-                providers
-            ).build(), SIGN_IN_RESULT_CODE
-        )
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == SIGN_IN_RESULT_CODE) {
-            val response = IdpResponse.fromResultIntent(data)
-            if (resultCode == Activity.RESULT_OK) {
-                // Successfully signed in user.
-                Log.i(
-                    TAG,
-                    "Successfully signed in user " +
-                            "${FirebaseAuth.getInstance().currentUser?.displayName}!"
-                )
-            } else {
-                // Sign in failed. If response is null the user canceled the sign-in flow using
-                // the back button. Otherwise check response.getError().getErrorCode() and handle
-                // the error.
-                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
-            }
-        }
-    }
 }
--- app/src/main/java/com/ciu196/android/monitored_wellbeing/MainFragment.kt
+++ app/src/main/java/com/ciu196/android/monitored_wellbeing/MainFragment.kt
@@ -16,7 +16,10 @@
 
 package com.ciu196.android.monitored_wellbeing
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -24,16 +27,25 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.ciu196.android.monitored_wellbeing.databinding.FragmentMainBinding
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 
 class MainFragment : Fragment() {
+    companion object {
+        const val TAG = "MainFragment"
+        const val SIGN_IN_RESULT_CODE = 1001
+    }
 
     // Get a reference to the ViewModel scoped to this Fragment
     private val viewModel by viewModels<LoginViewModel>()
     private lateinit var binding: FragmentMainBinding
+
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -46,13 +58,34 @@ class MainFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        navController = findNavController()
         super.onViewCreated(view, savedInstanceState)
         observeAuthenticationState()
 
         binding.settingsButton.setOnClickListener {
             val action = MainFragmentDirections.actionMainFragmentToSettingsFragment()
-            findNavController().navigate(action)
+            navController.navigate(action)
         }
+
+        // Observe the authentication state so we can know if the user has logged in successfully.
+        // If the user has logged in successfully, bring them back to the home screen.
+        // If the user did not log in successfully, display an error message.
+        viewModel.authenticationState.observe(viewLifecycleOwner, Observer { authenticationState ->
+            when (authenticationState) {
+                LoginViewModel.AuthenticationState.AUTHENTICATED -> {
+                    val action = MainFragmentDirections.actionMainFragmentToChallengeFragment()
+                    navController.navigate(action)
+                }
+                LoginViewModel.AuthenticationState.INVALID_AUTHENTICATION -> Snackbar.make(
+                    view, requireActivity().getString(R.string.login_unsuccessful_msg),
+                    Snackbar.LENGTH_LONG
+                ).show()
+                else -> Log.e(
+                    LoginFragment.TAG,
+                    "Authentication state that doesn't require any UI change $authenticationState"
+                )
+            }
+        })
     }
 
     /**
@@ -78,7 +111,7 @@ class MainFragment : Fragment() {
 
                     binding.authButton.text = getString(R.string.login_button_text)
                     binding.authButton.setOnClickListener {
-                        findNavController().navigate(R.id.loginFragment)
+                        launchSignInFlow()
                     }
                 }
             }
@@ -93,5 +126,41 @@ class MainFragment : Fragment() {
                 Character.toLowerCase(fact[0]) + fact.substring(1)
             )
         )
+    }
+
+    private fun launchSignInFlow() {
+        // Give users the option to sign in / register with their email or Google account. If users
+        // choose to register with their email, they will need to create a password as well.
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        // Create and launch sign-in intent. We listen to the response of this activity with the
+        // SIGN_IN_RESULT_CODE code.
+        startActivityForResult(
+            AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(
+                providers
+            ).build(), SIGN_IN_RESULT_CODE
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == SIGN_IN_RESULT_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in user.
+                Log.i(
+                    TAG,
+                    "Successfully signed in user " +
+                            "${FirebaseAuth.getInstance().currentUser?.displayName}!"
+                )
+            } else {
+                // Sign in failed. If response is null the user canceled the sign-in flow using
+                // the back button. Otherwise check response.getError().getErrorCode() and handle
+                // the error.
+                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+            }
+        }
     }
 }
