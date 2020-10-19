e3f4440359b295d44b41091899d9b5acb91d808b
Kevin Solovjov <kevin.solovjov@gmail.com>
2020-10-19 19:15:25 +0200

    Imported geofences

---
+++ app/src/main/java/com/ciu196/android/geofence/GeofenceBroadcastReceiver.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.treasureHunt
+
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.example.android.treasureHunt.HuntMainActivity.Companion.ACTION_GEOFENCE_EVENT
+import com.google.android.gms.common.GooglePlayServicesUtil.getErrorString
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofenceStatusCodes
+import com.google.android.gms.location.GeofencingEvent
+
+/*
+ * Triggered by the Geofence.  Since we only have one active Geofence at once, we pull the request
+ * ID from the first Geofence, and locate it within the registered landmark data in our
+ * GeofencingConstants within GeofenceUtils, which is a linear string search. If we had  very large
+ * numbers of Geofence possibilities, it might make sense to use a different data structure.  We
+ * then pass the Geofence index into the notification, which allows us to have a custom "found"
+ * message associated with each Geofence.
+ */
+class GeofenceBroadcastReceiver : BroadcastReceiver() {
+    // ...
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val geofencingEvent = GeofencingEvent.fromIntent(intent)
+        if (geofencingEvent.hasError()) {
+           val errorMessage = errorMessage(context!!,geofencingEvent.errorCode)
+            Log.e(TAG, errorMessage)
+            return
+        }
+
+        // Get the transition type.
+        val geofenceTransition = geofencingEvent.geofenceTransition
+        Log.i(TAG, geofenceTransition.toString())
+        // Test that the reported transition was of interest.
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER || geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+
+            // Get the geofences that were triggered. A single event can trigger
+            // multiple geofences.
+
+            // Send notification and log the transition details.
+
+            //Log.i(TAG, geofenceTransition.toString())
+        } else {
+        }
+    }
+}
+
+private const val TAG = "GeofenceReceiver"
